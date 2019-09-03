adc79016cdf52928dff577420d1aa26047cabc50
pavel-jurasek-hypofriend-de <pavel@hypofriend.de>
2019-09-03 12:32:27 +0200

    keep stubs but remove segment

--- src/init.js
+++ src/init.js
@@ -6,56 +6,54 @@ export default function init (config, callback) {
     return
   }
 
-  (function() {
-    var
-        analytics = window.analytics = window.analytics || [];
-    if (!analytics.initialize)
-        if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice.");
-        else {
-            analytics.invoked = !0;
-            analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "page", "once", "off", "on"];
-            analytics.factory = function(t) {
-                return function() {
-                    var e = Array.prototype.slice.call(arguments);
-                    e.unshift(t);
-                    analytics.push(e);
-                    return
-                    analytics
-                }
-            };
-            for (var t = 0; t < analytics.methods.length; t++) {
-                var
-                    e = analytics.methods[t];
-                analytics[e] = analytics.factory(e)
-            }
-            analytics.load = function(t) {
-                var
-                    e = document.createElement("script");
-                e.type = "text/javascript";
-                e.async = !0;
-                e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
-                var
-                    n = document.getElementsByTagName("script")[0];
-                n.parentNode.insertBefore(e, n)
-            };
-            analytics.SNIPPET_VERSION = "3.1.0";
-            analytics.load(config.id);
-            // Make sure to remove any calls to `analytics.page()`!
-        }
+  (function(){
+
+    // Create a queue, but don't obliterate an existing one!
+    var analytics = window.analytics = window.analytics || [];
+
+    // Invoked flag, to make sure the snippet
+    // is never invoked twice.
+
+    // A list of the methods in Analytics.js to stub.
+    analytics.methods = [
+      'trackSubmit',
+      'trackClick',
+      'trackLink',
+      'trackForm',
+      'pageview',
+      'identify',
+      'reset',
+      'group',
+      'track',
+      'ready',
+      'alias',
+      'debug',
+      'page',
+      'once',
+      'off',
+      'on'
+    ];
+
+    // Define a factory to create stubs. These are placeholders
+    // for methods in Analytics.js so that you never have to wait
+    // for it to load to actually record data. The `method` is
+    // stored as the first argument, so we can replay the data.
+    analytics.factory = function(method){
+      return function(){
+        var args = Array.prototype.slice.call(arguments);
+        args.unshift(method);
+        analytics.push(args);
+        return analytics;
+      };
+    };
+
+    // For each of our methods, generate a queueing stub.
+    for (var i = 0; i < analytics.methods.length; i++) {
+      var key = analytics.methods[i];
+      analytics[key] = analytics.factory(key);
+    }
+
   })();
-
-  const poll = setInterval(function () {
-    if (!window.analytics) {
-      return
-    }
-
-    clearInterval(poll)
-
-    // the callback is fired when window.analytics is available and before any other hit is sent
-    if (callback && typeof callback === 'function') {
-      callback()
-    }
-  }, 10);
 
   return window.analytics
 }
